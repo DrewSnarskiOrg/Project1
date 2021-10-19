3b0401bb6812b433e76a13f2d13738c11dc7c695
DrewSnarski <asnarski1@hawk.iit.edu>
2021-10-19 15:03:32 -0500

    added js check to see if text was filled in

--- js/site.js
+++ js/site.js
@@ -4,6 +4,26 @@ jQuery(function($) {
   });
 
   document.getElementById("submit").addEventListener("click", function(submitted) {
-    
+
+    if(inputs()&&checks()){
+
+    }
+    inputs(){
+        fname=document.getElementById("fname").value;
+        lname=document.getElementById("lname").value;
+        email=document.getElementById("email").value;
+        dob=document.getElementById("dob").value;
+
+
+        if(!fname||!lname||!email||!dob){
+            alert("Please fill in all the fields.");
+        }
+
+    }
+
+    checks(){
+        
+
+    }
 
   }
